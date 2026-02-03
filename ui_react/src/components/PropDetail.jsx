29426abe58db798b3838a708bf265d929c847fc3
UneasyTag26-dot <noah.johnson0801@gmail.com>
2026-02-03 16:14:38 -0600

    Create PropDetail.jsx

---
+++ ui_react/src/components/PropDetail.jsx
@@ -0,0 +1,11 @@
+// Placeholder component if you wish to implement detailed view
+import React from 'react';
+
+export default function PropDetail() {
+  return (
+    <div>
+      <h2>Prop Detail</h2>
+      <p>Select a prop to view details.</p>
+    </div>
+  );
+}
